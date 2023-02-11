57968a8a94c87e7f0173ce46e4f29cc274cc23a0
Khushiyant <khushiyant2002@gmail.com>
2023-02-11 23:12:16 +0530

    feat(add): setup login auth app

---
+++ base/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def login_index(request):
+    return render(request, "login/login.html")
--- kerver/settings/base.py
+++ kerver/settings/base.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "base",
 ]
 
 MIDDLEWARE = [
@@ -61,7 +62,10 @@ ROOT_URLCONF = "kerver.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            os.path.join(BASE_DIR, "templates/"),
+            os.path.join(BASE_DIR, "templates/sections/"),
+            ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
