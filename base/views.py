08ca31d194477d2a6fe8ddc072bad78996287b98
Khushiyant <khushiyant2002@gmail.com>
2023-02-12 17:57:32 +0530

    chore: add css--default, main- login, base

--- base/views.py
+++ base/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def login_index(request):
-    return render(request, "login/login.html")
+    return render(request, "login/index.html")
