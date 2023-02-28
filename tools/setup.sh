67872021a49cbcbcae236a7adfdbc04dfa0befbb
Khushiyant <khushiyant2002@gmail.com>
2023-03-01 00:51:11 +0530

    chore:  setup script--automate venv

--- tools/setup.sh
+++ tools/setup.sh
@@ -3,6 +3,12 @@
 # Copy the commit-msg hook to the .git/hooks directory
 cp ./tools/commit-msg ./.git/hooks/
 
+# create a virtual environment
+virtualenv kb_venv
+
+# activate the virtual environment
+source kb_venv/bin/activate
+
 # pip install the requirements
 pip install --upgrade pip && pip install -r ./requirements/requirements.txt 
 
