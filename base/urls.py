6827cfd12b4c2cc94398087082d82aa3464798f3
Khushiyant <khushiyant2002@gmail.com>
2023-03-05 20:24:02 +0530

    feat(add):  route--404,500

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("login/", views.login_index, name="login"),
+    path("404/", views.error_404, name="404"),
+    path("500/", views.error_500, name="500"),
 ]
--- base/views.py
+++ base/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def login_index(request):
     return render(request, "login/index.html")
+
+def error_404(request):
+    return render(request, "404.html")
+
+def error_500(request):
+    return render(request, "500.html")
