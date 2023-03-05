80ba5b2191b8e34e3fa71c6340d5ff7bf99d5e47
Khushiyant <khushiyant2002@gmail.com>
2023-03-06 01:54:00 +0530

    chore:  finalize login--structure

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path("login/", views.login_index, name="login"),
     path("404/", views.error_404, name="404"),
     path("500/", views.error_500, name="500"),
--- base/views.py
+++ base/views.py
@@ -1,12 +1,24 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 
-# Create your views here.
+@login_required(login_url="/login/")
+def index(request):
+    return render(request, "home/index.html")
+
+
 def login_index(request):
+    if request.user.is_authenticated:
+        return render(request, "home/index.html")
     return render(request, "login/index.html")
+
 
 def error_404(request):
     return render(request, "404.html")
+
 
 def error_500(request):
     return render(request, "500.html")
