cd0c2d4cded5b47a9924de6321caf486259ed316
Khushiyant <khushiyant2002@gmail.com>
2023-04-07 18:54:43 +0530

    chore:  add static requirements

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("profile/", views.profile, name="profile"),
     path("login/", views.login_index, name="login_index"),
     path("register/", views.register_index, name="register_index"),
     path("404/", views.error_404, name="404"),
