ac3376807aa1fd422820158688c49ec3c0e6b8e2
Khushiyant <khushiyant2002@gmail.com>
2023-04-08 20:11:40 +0530

    chore:  add forum section

--- frontend/views.py
+++ frontend/views.py
@@ -2,12 +2,22 @@ from django.shortcuts import render
 import json
 import yaml
 
+
 def profile(request):
     return render(request, "profile/index.html")
 
-def index(request):
 
+def index(request):
     context = yaml.load(open("static/yaml/faqs.yaml", "r"), Loader=yaml.FullLoader)
+    # context["forum_results"] = [
+    #     {"title": "test", "detail": "dfsdfsdfsdfvdsfdfbsdvsdvsd"},
+    #     {"title": "test2", "detail": "dfsdfsdfsdfvdsfdfbsdvsdvsd"},
+    #     {"title": "test", "detail": "dfsdfsdfsdfvdsfdfbsdvsdvsd"},
+    #     {"title": "test2", "detail": "dfsdfsdfsdfvdsfdfbsdvsdvsd"},
+    #     {"title": "test", "detail": "dfsdfsdfsdfvdsfdfbsdvsdvsd"},
+    #     {"title": "test2", "detail": "dfsdfsdfsdfvdsfdfbsdvsdvsd"},
+    # ]
+
     return render(request, "home/index.html", context=context)
 
 
